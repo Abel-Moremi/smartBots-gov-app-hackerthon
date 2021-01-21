4ae9eaab51f5705506ea1d209704d942fba13d24
Abel-Moremi <abelmoremiz@gmail.com>
2021-01-21 14:14:49 +0200

    fix: try sqlite database

--- smartbotsDjangoBackend/settings.py
+++ smartbotsDjangoBackend/settings.py
@@ -79,15 +79,10 @@ WSGI_APPLICATION = 'smartbotsDjangoBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
