770be47c96fcd4519129b8eff2bd4a9e96f3feb2
Abel-Moremi <abelmoremiz@gmail.com>
2021-01-21 14:21:52 +0200

    fix: add staticfiles

--- smartbotsDjangoBackend/settings.py
+++ smartbotsDjangoBackend/settings.py
@@ -122,5 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 # Activate Django-Heroku.
 django_heroku.settings(locals())
