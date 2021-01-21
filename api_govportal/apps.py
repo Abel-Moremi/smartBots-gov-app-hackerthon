40b9b090a43b820ffafa3c17d03383936d492470
Abel-Moremi <abelmoremiz@gmail.com>
2021-01-21 14:09:49 +0200

    fix: NUKE the repo with a new Django install

--- api_govportal/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiGovportalConfig(AppConfig):
-    name = 'api_govportal'
--- smartbotsDjangoBackend/wsgi.py
+++ smartbotsDjangoBackend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for GovPortal project.
+WSGI config for smartbotsDjangoBackend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GovPortal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartbotsDjangoBackend.settings')
 
 application = get_wsgi_application()
